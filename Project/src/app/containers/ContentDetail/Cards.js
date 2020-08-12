ad87c0d09b251504389ab28d09541275299e5a8e
Eray Berat ATALAY <erayberat37@gmail.com>
2020-08-12 23:11:28 +0300

    ContentDetail edited

--- Project/src/app/containers/ContentDetail/Cards.js
+++ Project/src/app/containers/ContentDetail/Cards.js
@@ -1,38 +1,36 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import { Row } from 'react-bootstrap';
-import { Col } from 'react-bootstrap';
-import { Card } from 'react-bootstrap';
-import '../../../index.css';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card';
+
+
 
 class Cards extends React.Component {
     render() {
         return (
             <Container fluid="md">
-                <Row><Col><h2>Lokanta Alsancak</h2></Col></Row>
+                <Row>
+                    <h2 className="location_header_style" >Ülkem Kokoreç</h2>
+                </Row>
                 <Row>
                     <Col md={6} >
                         <Card class="content_grid_card" className="text-white text-right">
-                            <Card.Img class="content_grid_card_image" variant="top" src={require("../../images/oludeniz.jpg")} />
+                            <Card.Img class="content_grid_card_image" src={require("../../images/oludeniz.jpg")} />
                         </Card>
                     </Col>
                     <Col md={6}>
-                        <h3>Information</h3>
+                        <h3>Address:<br />Tepeüstü, Alemdağ Cd No:568, 34771 Dudullu Osb/Ümraniye/İstanbul</h3>
                     </Col>
                 </Row>
                 <Row className="card_deck_margin">
                     <Col md={6}>
                         <Row>
                             <Col>
-                                Foto Gelecek
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col md={9}>
-                                Harika bir yemek.
-                            </Col>
-                            <Col md={3}>
-                                5Yıldız
+                                <Card.Img className="image_box" src={require("../../images/menton.jpg")} />
+                                <Card.Img className="image_box" src={require("../../images/oludeniz.jpg")} />
+                                <Card.Img className="image_box" src={require("../../images/grand-canyon.jpg")} />
                             </Col>
                         </Row>
                         <Row>
@@ -43,16 +41,28 @@ class Cards extends React.Component {
                                 4Yıldız
                             </Col>
                         </Row>
+
                         <Row>
-                            <Col>
-                                <button>Yorumyap</button>
+                            <Col md={9}>
+                                Güzel bir yemek.
+                            </Col>
+                            <Col md={3}>
+                                4Yıldız
+                            </Col>
+
+                        </Row>
+                        <Row>
+                            <Col md={9}>
+                                <input type="text" class="form-control" placeholder="Write a comment" />
+                            </Col>
+                            <Col md={3}>
+                                <Button className="comment_button">Comment</Button>
                             </Col>
                         </Row>
-
                     </Col>
                     <Col md={6}>
                         <Card class="content_grid_card" className="text-white text-right">
-                            <Card.Img variant="top" class="content_grid_card_image" src={require("../../images/oludeniz.jpg")} />
+                            <Card.Img style={{ height: "300px" }} src={require("../../images/oludeniz.jpg")} />
                             <Card.ImgOverlay>
                                 <Card.Title>Google Comment</Card.Title>
                                 <Card.Text>Go And Enjoy!lar</Card.Text>
