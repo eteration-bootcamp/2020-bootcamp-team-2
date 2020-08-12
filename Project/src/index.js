e9486a13f2fb1861ab023260bfbc0ec1cd46f5e4
Eray Berat ATALAY <erayberat37@gmail.com>
2020-08-13 01:27:25 +0300

    node-sass

--- Project/src/index.js
+++ Project/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './bootstrap-override.scss';
 import App from './App';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
