873ab641afc8f44ada2fe40e2737675d93f154de
Eray Berat ATALAY <erayberat37@gmail.com>
2020-08-16 04:09:41 +0300

    Content Detail edited

--- Project/frontend/src/app/containers/ContentDetail/Cards.js
+++ Project/frontend/src/app/containers/ContentDetail/Cards.js
@@ -12,46 +12,56 @@ class Cards extends React.Component {
         return (
             <Container fluid="md">
                 <Row>
-                    <h2 className="location_header_style" >Ülkem Kokoreç</h2>
+                    <h2 className="location_header_style">Loop Cafe</h2>
                 </Row>
                 <Row>
                     <Col md={6} >
                         <Card class="content_grid_card" className="text-white text-right">
-                            <Card.Img class="content_grid_card_image" src={require("../../images/oludeniz.jpg")} />
+                            <Card.Img class="content_grid_card_image" src={"https://b.zmtcdn.com/data/pictures/8/18480208/0071ba5033cddbe529bf8ab7a9992b82.jpg?output-format=webp"} />
                         </Card>
                     </Col>
                     <Col md={6}>
-                        <h3>Address:<br />Tepeüstü, Alemdağ Cd No:568, 34771 Dudullu Osb/Ümraniye/İstanbul</h3>
+                        <h3>
+                            Kahve, Çay <br />
+                            Kadıköy Merkez <br />
+                            Açılış saati 09:00 <br />
+                        </h3>
                     </Col>
                 </Row>
                 <Row className="card_deck_margin">
                     <Col md={6}>
                         <Row>
-                            <Col>
-                                <Card.Img className="image_box" src={require("../../images/menton.jpg")} />
-                                <Card.Img className="image_box" src={require("../../images/oludeniz.jpg")} />
-                                <Card.Img className="image_box" src={require("../../images/grand-canyon.jpg")} />
-                            </Col>
+                            <Col><Card.Img className="image_box" src={"https://b.zmtcdn.com/data/pictures/8/18480208/3071b4f9f2be780e8b0b0369f39ab41b.jpg?output-format=webp"} /></Col>
+                            <Col><Card.Img className="image_box" src={"https://b.zmtcdn.com/data/pictures/8/18480208/873792551c2bb5e8353bcbd1bb77dce3.jpg?output-format=webp"} /></Col>
+                            <Col><Card.Img className="image_box" src={"https://b.zmtcdn.com/data/pictures/8/18480208/dfe7a51f3503f88284954311bccee74f.jpg?output-format=webp"} /> </Col>
                         </Row>
-                        <Row>
+
+                        <Row className="card_deck_margin">
                             <Col md={9}>
-                                Güzel bir yemek.
+                                En güzel kahveler burada yapılıyor.
                             </Col>
                             <Col md={3}>
-                                4Yıldız
+                                Birhan ÖZÇELİK
                             </Col>
                         </Row>
 
-                        <Row>
+                        <Row className="card_deck_margin">
                             <Col md={9}>
-                                Güzel bir yemek.
+                                Arkadaşlarınızla bulaşabileceğiniz güzel bir mekan.
                             </Col>
                             <Col md={3}>
-                                4Yıldız
+                                Güliz AKKAYA
                             </Col>
-
                         </Row>
-                        <Row>
+                        <Row className="card_deck_margin">
+                            <Col md={9}>
+                                Çalışanları harika.
+                            </Col>
+                            <Col md={3}>
+                                Merve YILDIRIM
+                            </Col>
+                        </Row>
+                        <Row className="card_deck_margin">
                             <Col md={9}>
                                 <input type="text" class="form-control" placeholder="Write a comment" />
                             </Col>
