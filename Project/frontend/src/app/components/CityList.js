d69e410731e598570b12c3585a8f8a9241a070dd
birhanozcelik <34404097+birhanozcelik@users.noreply.github.com>
2020-08-20 07:37:59 +0300

    location completed 2

--- Project/frontend/src/app/components/CityList.js
+++ Project/frontend/src/app/components/CityList.js
@@ -3,15 +3,12 @@ import { getCities } from '../../api/apiCalls';
 import Dropdown from 'react-bootstrap/Dropdown'
 
 
-<<<<<<< HEAD
+
 const CityList = (props) => {
     const [cityPage, setCityPage] = useState({ content: [] , last: true, number: 0  })
 
 
-=======
-const CityList = (props, onSelectCityId) => {
-    const [cityPage, setCityPage] = useState({ content: [] })
->>>>>>> 54854b2db903c137a152e0213f1e58fc78a4e321
+
     useEffect(() => {
         loadCities(props.countryId);
     }, [props.countryId]);
@@ -28,14 +25,10 @@ const CityList = (props, onSelectCityId) => {
 
     return (
         <div>
-<<<<<<< HEAD
             {content.map(city => {               
                 return <Dropdown.Item onClick={ () => props.setCity(city.id,city.cityName)}> {city.cityName}</Dropdown.Item>
                 //<CityView key={city.id} city={city} />               
-=======
-            {content.map(city => {
-                return <Dropdown.Item onClick={() => onSelectCityId(city.id)}> {city.cityName}</Dropdown.Item>
->>>>>>> 54854b2db903c137a152e0213f1e58fc78a4e321
+
             })}
         </div>
     )
--- Project/frontend/src/app/components/CountryList.js
+++ Project/frontend/src/app/components/CountryList.js
@@ -27,13 +27,11 @@ const CountryList = ({ onSelectCountryId }) => {
         <div>
 
             {content.map(country => {
-<<<<<<< HEAD
                 //return <CountryView key={country.id} country={country} />
                 
                 return <Dropdown.Item  onClick={ () => onSelectCountryId(country.id,country.countryName)}> {country.countryName} </Dropdown.Item>
-=======
-                return <Dropdown.Item onClick={() => onSelectCountryId(country.id)}> {country.countryName} </Dropdown.Item>
->>>>>>> 54854b2db903c137a152e0213f1e58fc78a4e321
+
+
             })}
 
         </div>
