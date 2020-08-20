54854b2db903c137a152e0213f1e58fc78a4e321
Eray Berat ATALAY <erayberat37@gmail.com>
2020-08-20 04:07:17 +0300

    Cityview, Countryview edited

--- Project/frontend/src/app/components/CityList.js
+++ Project/frontend/src/app/components/CityList.js
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { getCities } from '../../api/apiCalls';
-import CityView from './CityView';
 import Dropdown from 'react-bootstrap/Dropdown'
 
 
-const CityList = (props,onSelectCityId) => {
+const CityList = (props, onSelectCityId) => {
     const [cityPage, setCityPage] = useState({ content: [] })
-
-
     useEffect(() => {
-
         loadCities(props.countryId);
     }, [props.countryId]);
 
@@ -19,20 +15,17 @@ const CityList = (props,onSelectCityId) => {
             setCityPage({
                 content: [...response.data]
             })
-
-        } catch (error) {}
+        } catch (error) { }
     }
-
     const { content } = cityPage;
 
     return (
         <div>
             {content.map(city => {
-                return <Dropdown.Item onClick={ () => onSelectCityId(city.id)}> {city.cityName}</Dropdown.Item>
-                //<CityView key={city.id} city={city} />               
+                return <Dropdown.Item onClick={() => onSelectCityId(city.id)}> {city.cityName}</Dropdown.Item>
             })}
         </div>
-    );
+    )
 };
 
 export default CityList;
--- Project/frontend/src/app/components/CountryList.js
+++ Project/frontend/src/app/components/CountryList.js
@@ -1,28 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { getCountries } from '../../api/apiCalls';
-import CountryView from './CountryView';
 import Dropdown from 'react-bootstrap/Dropdown'
 
 const CountryList = ({ onSelectCountryId }) => {
     const [countryPage, setCountryPage] = useState({ content: [], last: true, number: 0 })
-
     useEffect(() => {
 
         loadCountries();
     }, []);
-    
-    
 
     const loadCountries = async (page) => {
         try {
             const response = await getCountries(page);
-            
-            setCountryPage(previousCountryPage => ({
-                ...response.data,
-                content: [...previousCountryPage.content, ...response.data.content],
-                
-            }))
-            
+            setCountryPage({
+                content: [...response.data.content],
+            })
+
         } catch (error) { }
     }
 
@@ -32,11 +25,9 @@ const CountryList = ({ onSelectCountryId }) => {
 
     return (
         <div>
-            
+
             {content.map(country => {
-                //return <CountryView key={country.id} country={country} />
-                
-                return <Dropdown.Item  onClick={ () => onSelectCountryId(country.id)}> {country.countryName} </Dropdown.Item>
+                return <Dropdown.Item onClick={() => onSelectCountryId(country.id)}> {country.countryName} </Dropdown.Item>
             })}
 
         </div>
